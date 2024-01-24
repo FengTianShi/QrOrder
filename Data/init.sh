7066a6921acbcfa1a395cf79ca11e3fa854d6ff1
FengTianShi <fts2470216@gmail.com>
2024-01-24 14:37:05 +0900

    Update init.sh

--- Data/init.sh
+++ Data/init.sh
@@ -6,7 +6,7 @@ sudo mkdir /$uuid
 sudo cp -r ./SQL/* /$uuid
 sudo chmod 777 -r /$uuid
 
-sudo -u postgres psql -U postgres -d postgres -c "\i $SQL_PATH/"DB.SQL"
-sudo -u postgres psql -U postgres -d qrorder -c "\i $SQL_PATH/"TABLE.SQL"
+sudo -u postgres psql -U postgres -d postgres -c "\i /$uuid/"DB.SQL"
+sudo -u postgres psql -U postgres -d qrorder -c "\i /$uuid/"TABLE.SQL"
 
 sudo rm -rf /$uuid
